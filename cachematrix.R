b4a44e2843dc977bd762e39dad6412b03992e437
elviraj <juanjo.elvira@gmail.com>
2017-11-26 23:03:16 +0100

    Cache Matrix assigment V1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function is a matrix constructor function, and the second function calculates
+## the inversed matrix and keeps it in cache.
 
-## Write a short comment describing this function
+## This function adds to a matrix the methods set/get the matrix, and the setinv/getinv
+## to set/get the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) im <<- inv
+  getinv <- function() im
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inversed matrix for a given matrix. If the inversed matrix 
+## is already calculated, it does not calculate it again, since it is already cached.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  im <- x$getinv()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data, ...)
+  x$setinv(im)
+  im
 }
